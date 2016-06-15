38dcb53dd532d50fa3e6df13ab413a075ecd287b
berkaybahceci@gmail.com <berkaybahceci@gmail.com>
2016-06-15 13:02:16 +0300

    karakter hareket ediyor/vuruluyor

--- Catch_Troll/Form1.Designer.cs
+++ Catch_Troll/Form1.Designer.cs
@@ -29,11 +29,128 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.target = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.skor = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sure = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.target)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // target
+            // 
+            this.target.Image = ((System.Drawing.Image)(resources.GetObject("target.Image")));
+            this.target.Location = new System.Drawing.Point(130, 123);
+            this.target.Name = "target";
+            this.target.Size = new System.Drawing.Size(72, 52);
+            this.target.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.target.TabIndex = 0;
+            this.target.TabStop = false;
+            this.target.Click += new System.EventHandler(this.target_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 435);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Skor :";
+            // 
+            // skor
+            // 
+            this.skor.AutoSize = true;
+            this.skor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.skor.Location = new System.Drawing.Point(59, 435);
+            this.skor.Name = "skor";
+            this.skor.Size = new System.Drawing.Size(14, 13);
+            this.skor.TabIndex = 2;
+            this.skor.Text = "0";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 400);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(684, 10);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(199, 435);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Süre :";
+            // 
+            // sure
+            // 
+            this.sure.AutoSize = true;
+            this.sure.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sure.Location = new System.Drawing.Point(246, 435);
+            this.sure.Name = "sure";
+            this.sure.Size = new System.Drawing.Size(14, 13);
+            this.sure.TabIndex = 5;
+            this.sure.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(597, 431);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Sıfırla";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(684, 466);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sure);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.skor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.target);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hedefi Vur";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.target)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox target;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label skor;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label sure;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Catch_Troll/Form1.cs
+++ Catch_Troll/Form1.cs
@@ -16,5 +16,35 @@ namespace Catch_Troll
         {
             InitializeComponent();
         }
+
+        int zaman;
+        Random rast = new Random();
+        int ilk, son;
+        int score = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            zaman = 0;
+            sure.Text = Convert.ToString(zaman);
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+
+            zaman = zaman + 1;
+            sure.Text = Convert.ToString(zaman);
+            ilk = rast.Next(600);
+            son = rast.Next(350);
+            target.Location = new System.Drawing.Point(ilk,son);
+        }
+
+        private void target_Click(object sender, EventArgs e)
+        {
+            score++;
+            skor.Text = Convert.ToString(score);
+        }
     }
 }
